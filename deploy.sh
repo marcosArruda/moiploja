a9022084d5a253563814b88827aef8d1394b505d
Marcos Arruda <marcos.f.arruda@gmail.com>
2017-04-09 20:02:04 -0300

    added shebang

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 JAR_FILE=/home/marcos/prog-apps/workspaces/workspace-moip/moiploja/moiploja-site/target/moiploja-site-1.0.jar
 
 scp $JAR_FILE marcos@marcosarruda.info:/home/marcos
